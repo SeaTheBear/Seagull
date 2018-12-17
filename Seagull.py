7990349ec47af9b72da0086a8c70dbcb4fcb5bd7
SeaTheBear <36560464+SeaTheBear@users.noreply.github.com>
2018-12-16 20:48:53 -0800

    Add files via upload

--- Seagull.py
+++ Seagull.py
@@ -1,3 +1,29 @@
-{
-	"folders": []
-}
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+
+TOKEN = 'NTI0MDY2MjQ5Mzk1OTI5MTAz.Dviq5A.2sixszdetiEQW56lf4AvS2HxTEs'
+
+bot = commands.Bot(command_prefix = '-') 
+
+@bot.event
+async def on_ready():
+    print('Seagull is ready to fly')
+
+@bot.event
+async def on_message(message):
+ author = message.author
+ content = message.content
+ print ('{}: {}'.format(author, content))
+
+@bot.command(pass_context=True)
+async def ban(ctx, member: discord.Member):
+    await bot.ban(member)
+    await bot.say('Banned {0}'.format(member.mention))
+    
+
+
+
+bot.run(TOKEN)    
